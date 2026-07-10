08a86d5e3bd01156b3de4dca081ab9862ee8d95a
frontendfuchs <adriana.letitia@yahoo.com>
2026-07-10 22:57:20 +0200

    feat(level): create level1 with background layers and enemies

--- models/world.class.js
+++ models/world.class.js
@@ -1,45 +1,14 @@
 import { BackgroundObject } from "./background-object.class.js";
+import { level1 } from "../levels/level1.js";
 import { Character } from "./character.class.js";
 import { Chicken } from "./chicken.class.js";
 import { Cloud } from "./cloud.class.js";
 
 export class World {
 character = new Character();
-enemies = [
-    new Chicken(),
-    new Chicken(),
-    new Chicken(),
-];
-clouds =[
-    new Cloud()
-];
-backgroundObjects = [
-
-    new BackgroundObject('assets/img/5_background/layers/air.png', -719),
-    new BackgroundObject('assets/img/5_background/layers/3_third_layer/2.png', -719),
-    new BackgroundObject('assets/img/5_background/layers/2_second_layer/2.png', -719),
-    new BackgroundObject('assets/img/5_background/layers/1_first_layer/2.png', -719),
-
-    new BackgroundObject('assets/img/5_background/layers/air.png', 0),
-    new BackgroundObject('assets/img/5_background/layers/3_third_layer/1.png', 0),
-    new BackgroundObject('assets/img/5_background/layers/2_second_layer/1.png', 0),
-    new BackgroundObject('assets/img/5_background/layers/1_first_layer/1.png', 0),
-    new BackgroundObject('assets/img/5_background/layers/air.png', 719),
-    new BackgroundObject('assets/img/5_background/layers/3_third_layer/2.png', 719),
-    new BackgroundObject('assets/img/5_background/layers/2_second_layer/2.png', 719),
-    new BackgroundObject('assets/img/5_background/layers/1_first_layer/2.png', 719),
-
-    new BackgroundObject('assets/img/5_background/layers/air.png', 719*2),
-    new BackgroundObject('assets/img/5_background/layers/3_third_layer/1.png', 719*2),
-    new BackgroundObject('assets/img/5_background/layers/2_second_layer/1.png', 719*2),
-    new BackgroundObject('assets/img/5_background/layers/1_first_layer/1.png', 719*2),
-    new BackgroundObject('assets/img/5_background/layers/air.png', 719*3),
-    new BackgroundObject('assets/img/5_background/layers/3_third_layer/2.png', 719*3),
-    new BackgroundObject('assets/img/5_background/layers/2_second_layer/2.png', 719*3),
-    new BackgroundObject('assets/img/5_background/layers/1_first_layer/2.png', 719*3),
-    
-    
-]
+enemies = level1.enemies;
+clouds =level1.clouds;
+backgroundObjects = level1.backgroundObjects;
 
 canvas;
 keyboard;
